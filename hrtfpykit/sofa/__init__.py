30e3f3479d36d1eaf5364029aee969804c8fe859
aalvmar <66091265+ArielAlvarez-Martinez@users.noreply.github.com>
2026-03-05 22:20:00 +0100

    WIP: Check SOFA Files

--- hrtfpykit/sofa/__init__.py
+++ hrtfpykit/sofa/__init__.py
@@ -1,14 +1,8 @@
 from .conventions import CONVENTIONS
-<<<<<<< HEAD
-=======
 from .check import check_hrtf
->>>>>>> developer
 
 
 __all__ = [
     "CONVENTIONS",
-<<<<<<< HEAD
-=======
     "check_hrtf",
->>>>>>> developer
 ]
