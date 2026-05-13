b907825cf3520a73e8b423c6752cfad5bbf091b4
aalvmar <66091265+ArielAlvarez-Martinez@users.noreply.github.com>
2026-05-13 03:28:20 +0200

    cl

--- hrtfpykit/sofa/__init__.py
+++ hrtfpykit/sofa/__init__.py
@@ -1,11 +1,8 @@
-__version__ = "1.0.0"
-
 from .check import check_sofa_against_conventions, check_sofa_security
 from .sofa import SOFA, load_sofa
 
 
 __all__ = [
-    "__version__",
     "SOFA",
     "check_sofa_against_conventions",
     "check_sofa_security",
